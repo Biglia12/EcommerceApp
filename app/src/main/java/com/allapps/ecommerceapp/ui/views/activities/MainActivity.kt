b190a2acc294fe6e7791a99c35567b93bf44b99a
Ezequiel Biglia <ezequielbiglia12@gmail.com>
2024-03-11 12:50:54 -0300

    view binding y packages

---
+++ app/src/main/java/com/allapps/ecommerceapp/ui/views/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.allapps.ecommerceapp.ui.views.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.allapps.ecommerceapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
