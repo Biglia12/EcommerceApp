efef44b921fba298486f9d4d6c3553b878ac2032
Ezequiel Biglia <ezequielbiglia12@gmail.com>
2024-03-13 12:53:38 -0300

    dseing sign up

--- app/src/main/java/com/allapps/ecommerceapp/ui/views/fragments/signup/SignUpFragment.kt
+++ app/src/main/java/com/allapps/ecommerceapp/ui/views/fragments/signup/SignUpFragment.kt
@@ -9,27 +9,22 @@ import android.view.ViewGroup
 import androidx.activity.addCallback
 import androidx.navigation.fragment.findNavController
 import com.allapps.ecommerceapp.R
+import com.allapps.ecommerceapp.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
 
     private val viewModel: SignUpViewModel by viewModels()
     private val navController by lazy { findNavController() }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
+    private lateinit var binding: FragmentSignUpBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-
-
+        binding = FragmentSignUpBinding.inflate(inflater)
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
             navController.navigateUp()
         }
 
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        return binding.root
     }
 }
